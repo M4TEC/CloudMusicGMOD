be626ec80027f301f366c2c755879548424cf73e
ArknightsTexas <texas@tenmahw.com>
2020-01-21 21:58:13 +0800

    Fix some UI bug

--- lua/autorun/cloudmusic.lua
+++ lua/autorun/cloudmusic.lua
@@ -4,7 +4,7 @@ local function Print(msg,color)
     if color == nil then color = DEF_COLOR end
     MsgC(DEF_COLOR,"[",Color(106,204,255),"CloudMusic",DEF_COLOR,"] ",color,msg,"\n")
 end
-local CLOUDMUSIC_VER = "1.5.0 Beta 20200121"
+local CLOUDMUSIC_VER = "1.5.0 Beta 20200121.01"
 if CLIENT then
     local CLOUDMUSIC_SETTING_FILE_VER = "1.2.0"
     CreateClientConVar("cloudmusic_verbose", "0", true, false, "启用网易云播放器啰嗦模式")
